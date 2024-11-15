2c969843d597df7f3d18206c45fc1e33f7c4e1fc
Eritson Julio Zarza <sonyerickson61@gmail.com>
2024-11-14 22:41:57 -0500

    second commit

--- src/main/java/com/example/vertebrados/Vertebrados.java
+++ src/main/java/com/example/vertebrados/Vertebrados.java
@@ -1,4 +1,41 @@
 package com.example.vertebrados;
 
 public class Vertebrados {
+    private int id_animal;
+    private String nombre_cientifico;
+    private String nombre_comun;
+    private String clasificacion;
+
+
+    public int getId_animal() {
+        return id_animal;
+    }
+
+    public void setId_animal(int id_animal) {
+        this.id_animal = id_animal;
+    }
+
+    public String getNombre_cientifico() {
+        return nombre_cientifico;
+    }
+
+    public void setNombre_cientifico(String nombre_cientifico) {
+        this.nombre_cientifico = nombre_cientifico;
+    }
+
+    public String getNombre_comun() {
+        return nombre_comun;
+    }
+
+    public void setNombre_comun(String nombre_comun) {
+        this.nombre_comun = nombre_comun;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,13 @@ module com.example.vertebrados {
     requires javafx.controls;
     requires javafx.fxml;
 
+    requires java.sql;
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
+    requires mysql.connector.j;
 
     opens com.example.vertebrados to javafx.fxml;
     exports com.example.vertebrados;
