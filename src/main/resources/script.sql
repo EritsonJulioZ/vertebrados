5b40b3422bf8b192a7edd065e1a6f94272e80b98
Eritson Julio Zarza <sonyerickson61@gmail.com>
2024-11-14 22:44:12 -0500

    third commit

--- src/main/resources/script.sql
+++ src/main/resources/script.sql
@@ -1,45 +1,14 @@
 create database vertebrados;
 
 use  vertebrados;
+create database vertebrados;
 
-CREATE TABLE IF NOT EXISTS Clasificacion (
-     id_clasificacion INT AUTO_INCREMENT PRIMARY KEY,
-     nombre VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Perfiles (
-    id_perfil INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    permisos VARCHAR(255)
-);
-
-
-CREATE TABLE IF NOT EXISTS Usuarios (
-    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    id_perfil INT,
-    FOREIGN KEY (id_perfil) REFERENCES Perfiles(id_perfil)
-);
+use  vertebrados;
 
 CREATE TABLE IF NOT EXISTS Animales (
     id_animal INT AUTO_INCREMENT PRIMARY KEY,
     nombre_cientifico VARCHAR(255) NOT NULL,
     nombre_comun VARCHAR(255),
-    id_clasificacion INT,
-    id_usuario INT,
-    FOREIGN KEY (id_clasificacion) REFERENCES Clasificacion(id_clasificacion),
-    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id_usuario)
+    clasificacion  VARCHAR(255),
+    usuario  VARCHAR(255)
 );
-
-CREATE TABLE IF NOT EXISTS Caracteristicas (
-   id_caracteristica INT AUTO_INCREMENT PRIMARY KEY,
-   id_animal INT,
-   dieta VARCHAR(255),
-    habitat VARCHAR(255),
-    esperanza_vida INT,
-    FOREIGN KEY (id_animal) REFERENCES Animales(id_animal)
-);
-
-
-
